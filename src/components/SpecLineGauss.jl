44476b78b89d3e3cf143ecd0b829340ffff543a3
Giorgio Calderone <giorgio.calderone@gmail.com>
2021-06-02 17:08:08 +0200

    Improved performances

--- src/components/SpecLineGauss.jl
+++ src/components/SpecLineGauss.jl
@@ -6,7 +6,6 @@ mutable struct SpecLineGauss <: AbstractComponent
     center::Parameter
     fwhm::Parameter
     voff::Parameter
-    index::Vector{Int}  # optimization
     spec_res_kms::Float64
     norm_integrated::Bool
 
@@ -15,7 +14,7 @@ mutable struct SpecLineGauss <: AbstractComponent
                   Parameter(center),
                   Parameter(3000),
                   Parameter(0),
-                  Vector{Int}(), 0., true)
+                  0., true)
 
         @assert center > 0
         out.norm.low = 0
@@ -28,27 +27,22 @@ mutable struct SpecLineGauss <: AbstractComponent
 end
 
 function prepare!(comp::SpecLineGauss, domain::Domain{1})
-    comp.index = collect(1:length(domain))
     return fill(NaN, length(domain))
 end
 
 function evaluate!(buffer, comp::SpecLineGauss, x::Domain{1},
                    norm, center, fwhm, voff)
-    buffer[comp.index] .= 0.
-    empty!(comp.index)
-
     x0 = center - (voff / 3.e5) * center
     sigma_line = fwhm / 2.355      / 3.e5 * center
     sigma_spec = comp.spec_res_kms / 3.e5 * center
     sigma = sqrt(sigma_line^2 + sigma_spec^2)
 
-    X = (x .- x0) ./ sigma
-    i = findall(abs.(X) .< 4)  # optimization
-    append!(comp.index, i)
-    buffer[i] .= norm * exp.(-X[i].^2 ./ 2)
-    if comp.norm_integrated
-        buffer[i] ./= sqrt(2pi) * sigma
-    end
+    map!(x -> begin
+         (abs.(x - x0) .> 4sigma)  &&  (return 0.)
+         ret = norm * exp(-((x - x0) / sigma)^2 / 2)
+         comp.norm_integrated  &&  (ret /= (sqrt(2pi) * sigma))
+         return ret
+         end, buffer, x.coords[:,1])
 end
 
 
