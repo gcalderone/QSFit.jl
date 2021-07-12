a8ee95c13aa114da666672c7bde648f4901c5176
Giorgio Calderone <giorgio.calderone@gmail.com>
2021-07-12 09:32:39 +0200

    Plot data before reducers

--- src/gnuplot.jl
+++ src/gnuplot.jl
@@ -2,6 +2,6 @@ import Gnuplot
 import Gnuplot.recipe
 
 function Gnuplot.recipe(res::QSFitResults)
-    out = [Gnuplot.recipe(res.pspec.data), Gnuplot.recipe(res.model)...]
+    out = [Gnuplot.recipe(res.model)..., Gnuplot.recipe((domain(res.model), res.pspec.data))]
     return reverse(out)
 end
