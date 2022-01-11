a878be7964619ac28eecb734e696c0c644f58a9b
Giorgio Calderone <giorgio.calderone@gmail.com>
2022-01-11 23:13:39 +0100

    Implemented voigt_fwhm; Updated example

--- src/components/SpecLineVoigt.jl
+++ src/components/SpecLineVoigt.jl
@@ -2,15 +2,25 @@ function voigt(x, σ, γ)
     faddeeva(z) = erfcx(-im * z)
     z = (x + im * γ) / σ / sqrt(2)
     return real(faddeeva(z)) / (σ * sqrt(2pi))
-    return ret
 end
+
+function voigt_fwhm(σ, γ)
+    # J.J.Olivero and R.L. Longbothum in Empirical fits to the Voigt line width: A brief review, JQSRT 17, P233, 1977
+    # http://snst-hu.lzu.edu.cn/zhangyi/ndata/Voigt_profile.html
+    fg = σ * 2.355
+    fl = 2 * γ
+    0.5346 * fl + sqrt(0.2166 * fl^2 + fg^2)
+    # fl * (0.5346 + sqrt(0.2166 + (fg/fl)^2))
+end
+
 #=
 @gp "unset grid" xr=[-10,10]
 x = -100:0.01:100
-σ = 1.53;  γ = 0. ;  vp = voigt.(x, σ, γ);  @gp :- x vp "w l"; println(int_tabulated(x, vp))
-σ = 1.30;  γ = 0.5;  vp = voigt.(x, σ, γ);  @gp :- x vp "w l"; println(int_tabulated(x, vp))
-σ = 0.01;  γ = 1.8;  vp = voigt.(x, σ, γ);  @gp :- x vp "w l"; println(int_tabulated(x, vp))
-σ = 1.  ;  γ = 1. ;  vp = voigt.(x, σ, γ);  @gp :- x vp "w l"; println(int_tabulated(x, vp))
+σ = 1.53;  γ = 0. ;  vp = voigt.(x, σ, γ);  @gp :- x vp "w l"; println(int_tabulated(x, vp)); print(voigt_fwhm(σ, γ) - QSFit.estimate_fwhm(x, vp))
+σ = 1.30;  γ = 0.5;  vp = voigt.(x, σ, γ);  @gp :- x vp "w l"; println(int_tabulated(x, vp)); print(voigt_fwhm(σ, γ) - QSFit.estimate_fwhm(x, vp))
+σ = 0.01;  γ = 1.8;  vp = voigt.(x, σ, γ);  @gp :- x vp "w l"; println(int_tabulated(x, vp)); print(voigt_fwhm(σ, γ) - QSFit.estimate_fwhm(x, vp))
+σ = 1.  ;  γ = 1. ;  vp = voigt.(x, σ, γ);  @gp :- x vp "w l"; println(int_tabulated(x, vp)); print(voigt_fwhm(σ, γ) - QSFit.estimate_fwhm(x, vp))
+σ = 1.  ;  γ = 5. ;  vp = voigt.(x, σ, γ);  @gp :- x vp "w l"; println(int_tabulated(x, vp)); print(voigt_fwhm(σ, γ) - QSFit.estimate_fwhm(x, vp))
 =#
 
 # ____________________________________________________________________
@@ -61,10 +71,9 @@ end
 #=
     x = Domain(500:1:1500.)
     comp = QSFit.SpecLineVoigt(1000.)
-    comp.fwhm_g.val = 3e4
-    comp.fwhm_l.val = 3e4
+    comp.fwhm.val  = 3e4
+    comp.gamma.val = 3e4
     ceval = GFit.CompEval(comp, x)
     evaluate!(ceval)
     @gp x[:] ceval.buffer ./ maximum(ceval.buffer) "w l"
 =#
-
