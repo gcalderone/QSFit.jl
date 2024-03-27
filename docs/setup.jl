537387d1a7aff8e407a47e83c717140fc9c879f7
Giorgio Calderone <giorgio.calderone@gmail.com>
2024-03-27 14:10:52 +0100

    First commit

---
+++ docs/setup.jl
@@ -0,0 +1,10 @@
+using Gnuplot, Dates
+Gnuplot.quitall()
+mkpath("assets")
+Gnuplot.options.term = "unknown"
+empty!(Gnuplot.options.init)
+push!( Gnuplot.options.init, linetypes(:Set1_5, lw=2.5, ps=1.5))
+function saveas(file)
+    Gnuplot.save(term="pngcairo size 600,400 fontscale 0.8", "assets/$(file).png")
+    nothing
+end
