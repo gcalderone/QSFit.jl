26f3c7513736ef176bf57cf9351ad72aa73c40d9
Giorgio Calderone <giorgio.calderone@gmail.com>
2024-11-07 00:28:01 +0100

    Updated

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test, QSFit, QSFit.QSORecipes, GModelFitViewer
 
 spec = Spectrum(Val(:SDSS_DR10), joinpath(QSFit.qsfit_data(), "test", "spec-0752-52251-0323.fits"))
-recipe = Recipe(Type1, redshift=0.3806, Av=3.1 * 0.06846)
+recipe = CRecipe{Type1}(redshift=0.3806, Av=3.1 * 0.06846)
 res = analyze(recipe, spec)
 display(res.bestfit)
 display(res.fitstats)
@@ -12,7 +12,7 @@ rm(GModelFitViewer.serialize_html(res))
 
 
 spec = Spectrum(Val(:SDSS_DR10), joinpath(QSFit.qsfit_data(), "test", "spec-2233-53845-0594.fits"))
-recipe = Recipe(Type1, redshift=0.0999, Av=3.1 * 0.0209587)
+recipe = CRecipe{Type1}(redshift=0.0999, Av=3.1 * 0.0209587)
 res = analyze(recipe, spec)
 display(res.bestfit)
 display(res.fitstats)
